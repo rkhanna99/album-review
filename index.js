deeb642618e6bfc7a7260c01c08f43ac0eae8380
rkhanna99 <59949597+rkhanna99@users.noreply.github.com>
2020-09-14 18:59:44 -0400

    Heroku support added

--- index.js
+++ index.js
@@ -249,6 +249,6 @@ app.get('/aboutme', function(req, res) {
 
 
 /*Listen on port 3000*/
-app.listen(3000, function() {
-    console.log('App listening on port 3000!');
-})
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
+});
